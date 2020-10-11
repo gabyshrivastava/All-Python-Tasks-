df533ecb29f131510605c273d3e8f181b26ffe10
gabyshrivastava <72261872+gabyshrivastava@users.noreply.github.com>
2020-10-11 19:39:07 +0530

    Task 1 and Task 2
    Task 1 - Builtin functions 
    Task 2 - Check if 'to' is present in 'Welcome to python'

---
+++ Task 2 .py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#check if "to" is present in "Welcome to python" 
+# given str1="Welcome to python"
+
+
+# In[2]:
+
+
+str1 = "Welcome to python" 
+
+
+# In[3]:
+
+
+print("to" in str1)
+
+
+# In[ ]:
+
+
+
+
